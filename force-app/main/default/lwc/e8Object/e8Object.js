241120101b4d6a3059366203f4d52bd40c60d819
zehraBulut <ozgurdestina@gmail.com>
2023-07-27 22:23:11 +0300

    in lwc object code is ınserted

---
+++ force-app/main/default/lwc/e8Object/e8Object.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+import { LightningElement } from 'lwc';
+export default class breakExample extends LightningElement {   
+    person = {
+        name: 'John Smith',
+        age: 35,
+        email: 'john.smith@example.com'
+      };
+}
+
+export default class E8Object extends LightningElement {}
