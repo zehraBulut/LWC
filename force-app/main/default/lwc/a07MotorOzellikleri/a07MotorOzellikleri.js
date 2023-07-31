e686445fe7d61a18346d420f134df5e95d53273a
zehraBulut <ozgurdestina@gmail.com>
2023-07-31 15:41:41 +0300

    Composition group...3 component-parent chıld RS...

---
+++ force-app/main/default/lwc/a07MotorOzellikleri/a07MotorOzellikleri.js
@@ -0,0 +1,8 @@
+import { LightningElement,api } from 'lwc';
+export default class A07MotorOzellikleri extends LightningElement {
+
+@api motorHacmi='1.8'
+@api motorGucu= 120
+@api yakitTuru='BENZIN'
+}
+
