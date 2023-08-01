5277e51c2205f84cb9c0075fac4a7e8fd50ad117
zehraBulut <ozgurdestina@gmail.com>
2023-08-02 00:02:24 +0300

    code is inserted

---
+++ force-app/main/default/lwc/a09trackDecoratorObject/a09trackDecoratorObject.js
@@ -0,0 +1,41 @@
+import { LightningElement, api , track} from 'lwc';
+
+export default class A09trackDecoratorObject extends LightningElement {
+
+   @api ad='Destina'
+
+    handleIsim(event){
+    this.ad=event.target.value
+    this.fullIsim.name=event.target.value
+
+    }
+
+    @api soyad='Ozgur'
+
+    handleSoyIsim(event){
+    this.soyad=event.target.value
+    this.fullIsim.lastName=event.target.value
+    }
+
+     @track
+     fullIsim = {
+       name: 'Makluube',
+         lastName: 'Sisman'
+
+   }
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+}
