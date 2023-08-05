7361e350dea078304c81fa35250dfc28b091e3da
zehraBulut <ozgurdestina@gmail.com>
2023-08-06 01:31:32 +0300

    I created custom event,I canSee how many times I clicked button,my aim is parent will listen toChild

--- force-app/main/default/lwc/a17eventListenerParent/a17eventListenerParent.js
+++ force-app/main/default/lwc/a17eventListenerParent/a17eventListenerParent.js
@@ -5,13 +5,16 @@ export default class A17eventListenerParent extends LightningElement {
     handleClick(){
     this.clickSayisi++
     }
+
   //JS Listener
   //querySelectorAll dersek tum dıv lerı dınlersın
   renderedCallback() {
     this.template.querySelector('c-a17event-js').addEventListener('click', this.handleClickFromChild); 
 }
+
    handleClickFromChild() {
     alert("bu alarm child comp ın dugmesıne basıldiginda calisir.."); 
     window.alert("heyooooo buda windows alert ie olustu...");
 }
+
 }
