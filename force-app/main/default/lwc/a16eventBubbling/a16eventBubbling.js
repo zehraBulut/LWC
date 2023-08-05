3ba7057aeb2826ddb069e5fd6de1b966d7447bbd
zehraBulut <ozgurdestina@gmail.com>
2023-08-05 20:11:48 +0300

    4 august class is done

---
+++ force-app/main/default/lwc/a16eventBubbling/a16eventBubbling.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+export default class A16eventBubbling extends LightningElement {
+
+    innerDiv =0
+    middleDiv=0
+    outerDiv =0
+
+    handleInnerDiv(){
+   this.innerDiv++
+    }
+    handleMiddleDiv(event){
+    this.middleDiv++
+    event.stopPropagation()
+    }
+    handleOuterDiv(){
+    this.outerDiv++
+
+    }
+   }
+
+
