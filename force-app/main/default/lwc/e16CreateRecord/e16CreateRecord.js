6e9feb024c9079ac312d8785db34d40e9c7f618a
zehraBulut <ozgurdestina@gmail.com>
2023-08-14 22:43:48 +0300

    .

--- force-app/main/default/lwc/e16CreateRecord/e16CreateRecord.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement } from 'lwc';
-export default class E16CreateRecord extends LightningElement {
-    successMessage;
-    successHandler(e){
-        this.successMessage=' new account has been created id: ' + e.detail.id;
-    }
-}
