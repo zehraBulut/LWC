2139a0c4bac264b12d3e635da8442f6c9ca98910
zehraBulut <ozgurdestina@gmail.com>
2023-08-21 18:58:46 +0300

    its done

--- force-app/main/default/lwc/practicesLwc/hwmyComponent/hwmyComponent.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class HwmyComponent extends LightningElement {
-
-@api firstName = 'Destina';
-@api strTitle = 'Welcome in Salesforce';
-@api showImage = false;
-@api imgUrl = '';
-}
